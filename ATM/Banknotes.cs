28ef2f98ec034f5eb3dcb5db354058b9de2ff63c
Edvard <Edvard@KOMPKRASAVCIKA>
2023-03-29 23:17:47 +0400

    Add project files.

---
+++ ATM/Banknotes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public static class ATM
+    {
+        public static int Banknote(int amount)
+        {
+
+            const int hundred = 100;
+            const int fifty = 50;
+            const int twenty = 20;
+            const int ten = 10;
+            const int five = 5;
+            const int one = 1;
+
+            if (amount >= hundred)
+            {
+                return hundred;
+            }
+            if (amount >= fifty)
+            {
+                return fifty;
+            }
+            if (amount >= twenty)
+            {
+                return twenty;
+            }
+            if (amount >= ten)
+            {
+                return ten;
+            }
+            if (amount >= five)
+            {
+                return five;
+            }
+            if (amount >= one)
+            {
+                return one;
+            }
+            return 0;
+        }
+    }
+}
---
+++ ATM/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a value that you would like to withdraw in $: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            
+            while (amount > 0)
+            {
+                int banknote = ATM.Banknote(amount);
+                Console.WriteLine(banknote + "$");
+                amount -= banknote;
+                counter++;
+            }
+            if (amount < 0)
+            {
+                Console.WriteLine("Am i joke to you? Enter a real value!");
+            }
+            else
+            {
+                if (counter == 0)
+                {
+                    Console.WriteLine("You're broke my friend :(");
+                }
+                else 
+                {
+                    Console.WriteLine($"You've got {counter} banknotes");
+                }
+            }
+        }
+    }
+}
